9f3ebb72db4e399ce9f9f686facc72a27c5aac03
hakam4444 <89973506+hakam4444@users.noreply.github.com>
2021-09-21 14:47:02 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pixal
+import trutle
+   def test_init():
+   pixal .init()
+
+   x = trutle.speed()
+   assert(s==0)
+
+   x = turtle.xcor()
+   assert(x==200)
+    print_hi(name):
